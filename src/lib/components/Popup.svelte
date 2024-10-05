c7778e95dd3a2aed96cc466345f6c675dd3cd9b1
Ampflower 🌺 <git@ampflower.gay>
2024-10-04 22:15:14 -0700

    Remove checkbox; makes it more intuitive to use on touchscreens

--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 	import { ChevronRightIcon } from 'svelte-feather-icons';
 
-	export let id: string;
 	export let label: string;
 </script>
 
 <div class="popup">
-	<input {id} type="checkbox" hidden />
-	<label for={id}>
+	<span>
 		{label}
 		<ChevronRightIcon class="inline" />
-	</label>
+	</span>
 	<slot />
 </div>
