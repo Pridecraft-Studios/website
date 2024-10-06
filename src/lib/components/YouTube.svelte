3a62d1d545915f26bfa79e15fc9bd1fa1c2a3b74
Ampflower 🌺 <git@ampflower.gay>
2024-10-06 03:24:49 -0700

    Make 'allow' configurable

--- src/lib/components/YouTube.svelte
+++ src/lib/components/YouTube.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	export let id: string;
 	export let title: string;
+	export let controls: string = '0';
+	export let src: string = `https://www.youtube-nocookie.com/embed/${id}?controls=${controls}`;
+	export let allow: string = 'clipboard-write; encrypted-media; picture-in-picture; web-share';
+	export let referrerpolicy: string = 'strict-origin-when-cross-origin';
 </script>
 
 <a href="https://youtu.be/{id}" class="video" {title}>
 	<!-- They took the gay away... -->
 	<img src="/assets/youtube-noiframe.avif" alt={title} hidden loading="lazy" />
 	<iframe
-		src="https://www.youtube-nocookie.com/embed/{id}?controls=0"
+		{src}
 		{title}
-		allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
-		referrerpolicy="strict-origin-when-cross-origin"
+		{allow}
+		{referrerpolicy}
 		allowfullscreen
 		loading="lazy"
 		width="640"
