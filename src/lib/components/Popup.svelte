f776c6d062e355ad8256358296a464b1aa758d56
Ampflower 🌺 <git@ampflower.gay>
2024-10-10 13:36:45 -0700

    Add id to Popup.svelte

--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -2,9 +2,10 @@
 	import { ChevronRightIcon } from 'svelte-feather-icons';
 
 	export let label: string;
+	export let id: string = null;
 </script>
 
-<div class="popup" tabindex="0" role="button" aria-pressed="false">
+<div class="popup" tabindex="0" role="button" aria-pressed="false" {id}>
 	<span>
 		{label}
 		<ChevronRightIcon class="inline" />
